8229e3a5bf55ccbf4049f14a4f9bd3746712178d
Franco Montechiani <fnmontechiani@gmail.com>
2025-08-27 19:40:49 -0300

    Creamos la vista para cuando no tenemos ninguna tarea

---
+++ src/components/TaskDashBoard.tsx
@@ -0,0 +1,10 @@
+export function TaskDashBoard() {
+  return (
+    <>
+      <div>
+        <h3 className="text-4xl font-black">Bienvenido a ToDo</h3>
+        <p>Crea, edita y administra tus tareas del día como más gustes...</p>
+      </div>
+    </>
+  );
+}
