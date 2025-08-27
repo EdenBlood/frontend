df2a44a6050787cd7c2409a227e79016332d3042
Franco Montechiani <fnmontechiani@gmail.com>
2025-08-27 19:41:30 -0300

    service editado con nuevos verbos

--- src/services/TaskService.ts
+++ src/services/TaskService.ts
@@ -27,10 +27,43 @@ export default {
 
     try {
       const { data } = await api.get(url);
-      console.log(data);
       const response = apiTaskListResponseSchema.safeParse(data);
-      console.log(response.data);
       if (response.success) return response.data.tasks;
+    } catch (error) {
+      if (isAxiosError(error) && error.response) {
+        throw new Error(error.response.data.error);
+      }
+    }
+  },
+
+  getTaskById: async ({ taskId }: { taskId: string }) => {
+    const url = `/tasks/${taskId}`;
+
+    try {
+      const { data } = await api.get(url);
+
+      const response = apiTaskFullResponseSchema.safeParse(data);
+      if (response.success) return response.data.task;
+    } catch (error) {
+      if (isAxiosError(error) && error.response) {
+        throw new Error(error.response.data.error);
+      }
+    }
+  },
+
+  updateTask: async ({
+    taskId,
+    task,
+  }: {
+    taskId: string;
+    task: TaskFormData;
+  }) => {
+    const url = `/tasks/${taskId}`;
+    try {
+      const { data } = await api.put(url, task);
+
+      const response = apiTaskFullResponseSchema.safeParse(data);
+      if (response.success) return response.data;
     } catch (error) {
       if (isAxiosError(error) && error.response) {
         throw new Error(error.response.data.error);
